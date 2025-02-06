3ca4808ad25f07135cd9a7448af071bce6c7e841
ohmilei2333 <121669448+ohmilei2333@users.noreply.github.com>
2025-02-06 14:16:11 +0800

    Add files via upload

---
+++ 1.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+	cout<<"牛逼大模型 ver114.5.14"<<endl;
+	char a[1000000005]={""};
+	while(true){
+		cout<<"请输入要提问的内容：";
+		cin>>a;
+		a[1000000005]=' ';
+		for(int i=0;i<1000000000;i++){
+			;
+		}
+		cout<<endl<<"网络开小差了，请稍后再试！"<<endl;
+	}
+	return 0;
+}
+
